e4de010cd6ed30f48338e63305cce16e704badba
mohammadmiZAN <takbir.jcd@gmail.com>
2021-12-07 23:27:05 +0600

    clock update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,35 @@
 let clockContent
 const clock = () => {
     let date = new Date()
-    let hours = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
-    let format = hours >= 12 ? 'pm' : 'am'
-    hours = hours % 12
-    hours = hours ? hours : 12 // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes
-    seconds = seconds < 10 ? '0' + seconds : seconds
+    // let hours = date.getHours()
+    // let minutes = date.getMinutes()
+    // let seconds = date.getSeconds()
+    // let format = hours >= 12 ? 'pm' : 'am'
+    // hours = hours % 12
+    // hours = hours ? hours : 12 // the hour '0' should be '12'
+    // minutes = minutes < 10 ? '0' + minutes : minutes
+    // seconds = seconds < 10 ? '0' + seconds : seconds
     // let time = `${hours}:${minutes}:${seconds}${format}`
     let time = date.toLocaleTimeString()
     clockContent = document.getElementById('time')
     clockContent.innerText = time;
     setInterval(clock, 1000)
-
-
 }
-clock()
+
 let isTrue = true;
 let toggle = document.getElementById('toggle')
-window.addEventListener('load',(e)=>{
-    toggle.addEventListener('click', (e) => {
-        e.preventDefault()
-        isTrue =!isTrue;
-        if (isTrue===false) {
-            clockContent.style.visibility = "hidden";
-            toggle.innerText = 'Show';
-        }else{
-            clockContent.style.visibility = "visible";
-            toggle.innerText = 'Hide';
-        }
-        console.log(isTrue)
+toggle.addEventListener('click', (e) => {
+    e.preventDefault()
+    isTrue =!isTrue;
+    if (isTrue===false) {
+        clockContent.style.visibility = "hidden";
+        toggle.innerText = 'Show';
+    }else{
+        clockContent.style.visibility = "visible";
+        toggle.innerText = 'Hide';
+    }
+    console.log(isTrue)
+    
 
-    })
+
 })
-
