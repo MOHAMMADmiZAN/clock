706227b408555ec270f9ab0361947a5d305392ce
mohammadmiZAN <takbir.jcd@gmail.com>
2021-12-07 22:28:01 +0600

    toggle add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
+let clockContent
 const clock = () => {
     let date = new Date()
-
     let hours = date.getHours()
     let minutes = date.getMinutes()
     let seconds = date.getSeconds()
@@ -9,14 +9,30 @@ const clock = () => {
     hours = hours ? hours : 12 // the hour '0' should be '12'
     minutes = minutes < 10 ? '0' + minutes : minutes
     seconds = seconds < 10 ? '0' + seconds : seconds
-    let time = `${hours}:${minutes}:${seconds}${format}`
-    // let time = date.toLocaleTimeString()
-    let dateLocal = date.toLocalDateString()
-    let clockContent = document.querySelector('#time')
-    let dateContent = document.querySelector('#date')
-    clockContent.innerText = time
-    dateContent.innerText = dateLocal
+    // let time = `${hours}:${minutes}:${seconds}${format}`
+    let time = date.toLocaleTimeString()
+    clockContent = document.getElementById('time')
+    clockContent.innerText = time;
     setInterval(clock, 1000)
+
 
 }
 clock()
+let isTrue = true;
+let toggle = document.getElementById('toggle')
+window.addEventListener('load',(e)=>{
+    toggle.addEventListener('click', (e) => {
+        e.preventDefault()
+        isTrue =!isTrue;
+        if (isTrue===false) {
+            clockContent.style.visibility = "hidden";
+            toggle.innerText = 'Show';
+        }else{
+            clockContent.style.visibility = "visible";
+            toggle.innerText = 'Hide';
+        }
+        console.log(isTrue)
+
+    })
+})
+
